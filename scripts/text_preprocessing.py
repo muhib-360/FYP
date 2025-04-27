b0c024d6611f59ed97699f6a744baffbaae57828
Muhib Ali <138896729+muhib-360@users.noreply.github.com>
2025-04-27 13:18:28 +0500

    Update text_preprocessing.py

--- scripts/text_preprocessing.py
+++ scripts/text_preprocessing.py
@@ -1,2 +1,4 @@
-Here's the link for colab notebook. You can access here : [(https://colab.research.google.com/drive/1_0aFc_C3WKF4l29lMfkMHMH65ZBWNvje?usp=sharing)]
+Here's the link for colab notebook. You can access here : 
+
+https://colab.research.google.com/drive/1_0aFc_C3WKF4l29lMfkMHMH65ZBWNvje?usp=sharing
 
